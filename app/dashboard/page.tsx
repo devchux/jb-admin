1550ef8edec11f5ea0cf9e69cc2ea4e17fd90607
Dev-team-plantec <39128641+JocheAttah@users.noreply.github.com>
2025-08-05 23:49:32 +0100

    Create page.tsx

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <>
+      <div className="font-light text-2xl">
+        {' '}
+        <span className="text-[#6E6D6D]">Welcome, </span>Admin
+      </div>
+    </>
+  );
+};
+
+export default Dashboard;
